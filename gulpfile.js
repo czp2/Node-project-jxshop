bb8794e1214ee17c45286267e3e2f01f292b8a3c
czp2 <1127731529@qq.com>
2021-08-09 03:49:00 +0800

    Sat order

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,37 @@
 // 导入模块
-const gulp = require('gulp')            // gulp模块，用于创建任务的
-const uglify = require('gulp-uglify')   // 压缩JS 去空格去注释
-const babel = require('gulp-babel')     // es6转es5 浏览器兼容 babel
-const rev = require('gulp-rev')         // 加随机名的，内容修改就重新生成新的随机名
+const gulp = require('gulp') // gulp模块，用于创建任务的
+const uglify = require('gulp-uglify') // 压缩JS 去空格去注释
+const babel = require('gulp-babel') // es6转es5 浏览器兼容 babel
+const rev = require('gulp-rev') // 加随机名的，内容修改就重新生成新的随机名
 const revCollector = require('gulp-rev-collector'); // 根据temp目录记录的规则 替换 html路径
-const clean = require('gulp-clean')     // 删除目录文件的
+const clean = require('gulp-clean') // 删除目录文件的
 const minifyCSS = require('gulp-minify-css') // 压缩CSS 去空格去注释的
 const imagemin = require('gulp-imagemin') // 压缩图片
-const htmlmin = require('gulp-htmlmin')  // 压缩HTML 去空格去注视
- 
+const htmlmin = require('gulp-htmlmin') // 压缩HTML 去空格去注视
+
 
 // 打包HTML任务
 // gulp.task('html', () => {
 gulp.task('html', over => {
 
     var options = {
-        removeComments: true,//清除HTML注释
-        collapseWhitespace: true,//压缩HTML
-        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
-        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
-        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
-        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
-        minifyJS: true,//压缩页面JS 
-        minifyCSS: true//压缩页面CSS
-    }; 
-    gulp    
+        removeComments: true, //清除HTML注释
+        collapseWhitespace: true, //压缩HTML
+        collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+        minifyJS: true, //压缩页面JS 
+        minifyCSS: true //压缩页面CSS
+    };
+    gulp
         // .src('./src/html/*.html')
         // .src(['temp/**/*.json', './src/html/*.html'])
         .src(['temp/**/*.json', './src/*.html'])
-        .pipe( revCollector() )
-        .pipe( htmlmin(options) )
+        .pipe(revCollector())
+        .pipe(htmlmin(options))
         // .pipe( gulp.dest('./dist/html/') )
-        .pipe( gulp.dest('./dist/') )
+        .pipe(gulp.dest('./dist/'))
 
 
     over()
@@ -41,9 +41,9 @@ gulp.task('html', over => {
 // gulp.task('imgs', () => {
 gulp.task('imgs', over => {
     gulp
-        .src('./src/imgs/*.*') 
-        .pipe( imagemin() )  
-        .pipe( gulp.dest('./dist/imgs/') )
+        .src('./src/imgs/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imgs/'))
 
     // over()
 
@@ -57,15 +57,15 @@ gulp.task('imgs', over => {
 gulp.task('css', over => {
     gulp
         .src('./src/css/*.css')
-        .pipe( minifyCSS() )
-        .pipe( rev() )
-        .pipe( gulp.dest('./dist/css/') )
+        .pipe(minifyCSS())
+        .pipe(rev())
+        .pipe(gulp.dest('./dist/css/'))
         // ------------------------------
         // 每次打包
         // 将css对应的随机名保存到json中
         // ------------------------------
         .pipe(rev.manifest())
-        .pipe(gulp.dest('temp/css')) 
+        .pipe(gulp.dest('temp/css'))
 
     over()
 })
@@ -74,12 +74,13 @@ gulp.task('css', over => {
 const fs = require('fs');
 // gulp.task('clean', () => {
 gulp.task('clean', over => {
-    
-    if (fs.existsSync('dist/'))
-    {
-        gulp.src('dist/',{read: false})
-        .pipe(clean());
-    } 
+
+    if (fs.existsSync('dist/')) {
+        gulp.src('dist/', {
+                read: false
+            })
+            .pipe(clean());
+    }
 
 
     // over()
@@ -96,17 +97,19 @@ gulp.task('js', over => {
     // gulp.src('./src/js/a.js')
     // gulp.src(['./src/js/a.js', './src/js/b.js'])
     gulp.src('./src/js/*.js')
-    // .pipe(模块名())
-    .pipe(babel({"presets": ["env"]}))
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe( gulp.dest('./dist/js/') )
-    // ------------------------------
-    // 每次打包
-    // 将js对应的随机名保存到json中
-    // ------------------------------
-    .pipe(rev.manifest())
-	.pipe(gulp.dest('temp/js')) 
+        // .pipe(模块名())
+        .pipe(babel({
+            "presets": ["env"]
+        }))
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest('./dist/js/'))
+        // ------------------------------
+        // 每次打包
+        // 将js对应的随机名保存到json中
+        // ------------------------------
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('temp/js'))
 
     over()
 })
@@ -143,26 +146,51 @@ gulp.task('build', gulp.series('clean', 'js', 'css', 'imgs', 'html', () => {
 
 
 // 通过node搞web服务器开发的时候使用
-const server = require('browser-sync').create(); 
-const watch = require('gulp-watch'); 
-gulp.task('serve', () => {
-    
+const server = require('browser-sync').create();
+const watch = require('gulp-watch');
+const {
+    createProxyMiddleware
+} = require('http-proxy-middleware');
+gulp.task('serve', function () {
     server.init({
         server:'./src',
-        port: 80
+        port: 3000
+    })
+    // server.init({
+    //     server: {
+    //         baseDir: 'src',
+    //         middleware: [
+    //             createProxyMiddleware('/api', {
+    //                 target: 'http://kg.zhaodashen.cn/',
+    //                 changeOrigin: true,
+    //                 secure: false,
+    //                 pathRewrite: {
+    //                     '^/api':''
+    //                 }
+    //             }),
+    //             // ...
+    //         ]
+    //     },
+    //     port: 3000
+    // })
+
+    watch('src/*.html', function () {
+        console.log('html change')
+        server.reload()
     })
 
-    // 监控文件修改
-    watch('src/*.html', function() {
-        console.log('你修改HTML文件了')
+    watch('src/style/*', function () {
+        console.log('style change')
         server.reload()
     })
-    watch('src/style/*.css', function() {
-        console.log('你修改CSS文件了')
+
+    watch('src/js/*', function () {
+        console.log('js change')
         server.reload()
-    }) 
-    watch('src/js/*.js', function() {
-        console.log('你修改JS文件了')
+    })
+
+    watch('src/api/*', function () {
+        console.log('api change')
         server.reload()
     })
 })
---
+++ src/api/index.js
@@ -0,0 +1,94 @@
+$(function () {
+    //猜你喜欢
+    $('.floor1 .goodslist div:first ul').html('')
+    $.get('http://kg.zhaodashen.cn//v1/goods/like.jsp', {}, res => {
+        // console.log(res);
+        let html = ''
+        $.each(res.data, (index, item) => {
+            // console.log(item);
+            //拼接
+            html += `
+            <li>
+                <dl>
+                    <dt><a href=""><img src="http://tmp00001.zhaodashen.cn/${item.goods_img}" alt="" /></a></dt>
+                    <dd><a href="">${item.goods_name}</a></dd>
+                    <dd><span>售价：</span> <strong>￥${item.market_price}</strong></dd>
+                </dl>
+            </li>
+            `
+        })
+        //覆盖
+        $('.floor1 .goodslist div:first ul').html(html)
+    }, 'json')
+
+    //热卖商品等
+    let goodsTypes = {
+        '热卖商品': 'hot',
+        '推荐商品': 'best',
+        '新品上架': 'new'
+    }
+    getGoodList(goodsTypes['热卖商品'], 0)
+    $('.guide_content h2 span').mouseover(function () {
+        let spanIndex = $(this).index();
+        getGoodList(goodsTypes[$(this).text()], spanIndex)
+    })
+
+    function getGoodList(type, spanIndex) {
+        $.get('http://kg.zhaodashen.cn//v1/goods/index.jsp', {
+            type,
+        }, res => {
+            let html = ``
+            $.each(res.data.list, (index, item) => {
+                html += `
+					<li>
+						<dl>
+							<dt><a href="./goods.html?id=${item.goods_id}"><img src="http://tmp00001.zhaodashen.cn/${item.goods_img}" alt="" /></a></dt>
+							<dd><a href="./goods.html?id=${item.goods_id}">${item.goods_name}</a></dd>
+							<dd><span>售价：</span><strong> ￥${item.shop_price}</strong></dd>
+						</dl>
+					</li>
+				`
+            })
+            // 放到页面中   
+            $(`.guide_wrap div:eq(${spanIndex}) ul`).html(html)
+        }, 'json')
+    }
+
+    //商品左侧列表
+    $('.category .cat_bd').html('')
+    $.get('http://kg.zhaodashen.cn/v1/category/index.jsp', {}, res => {
+        // console.log(res.data);
+        let html = ``
+			$.each(res.data, (index, menu) => {
+				if (index > 12) return
+				// 一级
+				html += `<div class="cat item1">`
+				html +=
+					`<h3><a href="${menu.url ? menu.url : './list.html?k='+menu.keywords}">${menu.cat_name}</a> <b></b></h3>`
+				html += `<div class="cat_detail">`
+				// 二级
+				$.each(menu.children, (index, menu2) => {
+                    if (index > 8) return
+					html += `<dl class="dl_1st">`
+					html +=
+						`<dt><a href="${menu2.url ? menu2.url : './list.html?k='+menu2.keywords}">${menu2.cat_name}</a></dt>`
+					html += `<dd>`
+					// 三级
+					$.each(menu2.children, (index, menu3) => {
+						html +=
+							`<a href="${menu3.url ? menu3.url : './list.html?cat='+menu3.cat_id}">${menu3.cat_name}</a>`
+					})
+					html += `</dd>`
+					html += `</dl>`
+				})
+				html += `</div>`
+				html += `</div>`
+			})
+			// 放到页面中
+			$('.category .cat_bd').html(html)
+    }, 'json')
+
+    //定位
+    let locCity = sessionStorage.getItem('location') || '请选择城市'
+    $('#location').text(locCity)
+})
